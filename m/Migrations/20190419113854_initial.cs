68e184450dec358630f61716acba1371e2c0b642
kogo <saba.koguashvili1999@mail.ru>
2019-04-21 13:04:50 +0400

    bd

---
+++ m/Migrations/20190419113854_initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace m.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VideoGames",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Platform = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoGames", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VideoGames");
+        }
+    }
+}
---
+++ m/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace m
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            using(VideoGamesDatabaseContext context = new VideoGamesDatabaseContext())
+            {
+                context.VideoGames.Add(new VideoGame() { Title = "Kogoia" , Platform = 1999});
+                context.SaveChanges();
+            }
+        }
+    }
+
+    public class VideoGame {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Platform { get; set; }
+    }
+
+    public class VideoGamesDatabaseContext : DbContext
+    {
+        public DbSet<VideoGame> VideoGames {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        optionsBuilder.UseSqlServer("Server=localhost; Database=VideoGames;User=SA; Password=<YourStrong!Passw0rd>");
+        }
+    }
+}
