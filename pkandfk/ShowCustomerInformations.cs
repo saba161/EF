c582ac026de1e96d24c6fe42009ad08fb24f6879
kogo <saba.koguashvili1999@mail.ru>
2019-05-22 22:36:36 +0400

    ef

--- pkandfk/ShowCustomerInformations.cs
+++ pkandfk/ShowCustomerInformations.cs
@@ -16,10 +16,10 @@ public class ShowCustomerInformations
         using(Technic context = new Technic())
         {
             //var findedRecords = context.Customers.Where(r => r.Email == this.email).ToList();
-            var findedRecords = context.Orders.Where(o => o.Customer.Email == this.email).ToList();
-            if(findedRecords.Count() > 0)
+            var findedRecords = context.Customers.Where(o => o.Email == this.email).FirstOrDefault();
+            if(findedRecords != null)
             {
-                foreach(var item in findedRecords)
+                foreach(var item in findedRecords.Orders)
                 {
                     var id = item.Id;
                     Console.WriteLine($"Id: {item.Id}, ProductName: {item.ProductName}, Quantity: {item.Quantity}, PurchaseDate: {item.PurchaseDate}");
